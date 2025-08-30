b21fcd74a10a3dd305fff651dae3bd1a328a1e5e
Aditya Belsare <71834027+belsareaditya@users.noreply.github.com>
2025-08-30 17:21:23 +0530

    Update variables.tf

--- Terraform-Eks-Creation/variables.tf
+++ Terraform-Eks-Creation/variables.tf
@@ -5,13 +5,13 @@
 variable "aws_region" {
   description = "AWS region where resources will be created"
   type        = string
-  default     = "us-west-2"
+  default     = "ap-south-1"
 }
 
 variable "cluster_name" {
   description = "Name of the EKS cluster"
   type        = string
-  default     = "retail-store"
+  default     = "Vault_Guard"
 }
 
 variable "environment" {
